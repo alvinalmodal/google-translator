10adbe62be423c64451ec8fff2a6ca23b93b663e
alvin almodal <almodalalvin@gmail.com>
2021-06-21 21:18:03 +0800

    added first iteration for google translator app

--- pages/api/translate.js
+++ pages/api/translate.js
@@ -3,39 +3,12 @@
 const _ = require("lodash");
 const request = require("request-promise");
 
-module.exports = function () {
-  let opts, text, sourceLang, targetLang, callback;
-  const args = [].slice.call(arguments);
-  if (_.isPlainObject(args[0])) {
-    opts = args[0];
-    text = opts.text;
-    sourceLang = opts.source || opts.src || opts.sl || opts.sourceLang;
-    targetLang = opts.target || opts.tgt || opts.tl || opts.targetLang;
-    callback = args[1];
-  } else if (_.isString(args[0])) {
-    text = args[0];
-    if (_.isString(args[1])) {
-      if (_.isString(args[2])) {
-        sourceLang = args[1];
-        targetLang = args[2];
-        callback = args[3];
-      } else {
-        sourceLang = "auto";
-        targetLang = args[1];
-        callback = args[2];
-      }
-    }
-  } else {
-    throw new Error("Invalid arguments");
-  }
-
-  if (!text) {
-    throw new Error("Need a text to translate");
-  }
-  if (!targetLang) {
-    throw new Error("Need a targetLang to translate to");
-  }
-  callback = _.isFunction(callback) ? callback : _.noop;
+const translate = (sourceLang, targetLang, text) => {
+  const data = {
+    sl: sourceLang,
+    tl: targetLang,
+    q: text,
+  };
 
   const url =
     "https://translate.google.com/translate_a/single" +
@@ -43,12 +16,6 @@ module.exports = function () {
     targetLang +
     "&ie=UTF-8" +
     "&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e";
-
-  const data = {
-    sl: sourceLang,
-    tl: targetLang,
-    q: text,
-  };
 
   return request({
     method: "POST",
@@ -76,7 +43,6 @@ module.exports = function () {
 
       const translation = new Translation(json);
 
-      callback(translation);
       return translation;
     })
     .catch(function (err) {
@@ -87,7 +53,16 @@ module.exports = function () {
       newError.statusCode = err.statusCode;
       newError.url = url;
       newError.body = err.error;
-      callback(newError);
       throw newError;
     });
+};
+
+export default async (req, res) => {
+  const { sourceLang, targetLang, query } = req.query;
+  try {
+    let translation = await translate(sourceLang, targetLang, query);
+    res.status(200).json(translation);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { createDataList } from "../services/supportLanguages";
 
 export default function Home() {
-  const [language, setLanguage] = useState({});
+  const [sourceLang, setSourceLang] = useState("tl");
+  const [targetLang, setTargetLang] = useState("en");
+  const [query, setQuery] = useState("");
+  const [translatedQuery, setTranslatedQuery] = useState("");
+
+  const translate = async () => {
+    console.log(
+      sourceLang,
+      targetLang,
+      query,
+      `/api/translate?sourceLang=${sourceLang}&targetLang=${targetLang}&query=${query}`
+    );
+    const res = await fetch(
+      `/api/translate?sourceLang=${sourceLang}&targetLang=${targetLang}&query=${query}`
+    );
+    const data = await res.json();
+    mapAPIResponse(data);
+  };
+
+  const mapAPIResponse = ({ sentences }) => {
+    if (sentences) {
+      let translation = sentences.map((sentence) => sentence.trans).join("");
+      setTranslatedQuery(translation);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -14,8 +39,39 @@ export default function Home() {
           content="This is a development practice challenge for topcoder by Alvin B. Almodal"
         />
       </Head>
-
-      <main className={styles.main}>H1 Test</main>
+      <h1 className={styles.header}>Google Translator</h1>
+      <main className={styles.main}>
+        <input
+          list="supported-languages"
+          name="sourceLang"
+          id="sourceLang"
+          onChange={(e) => setSourceLang(e.target.value)}
+        />
+        <input
+          list="supported-languages"
+          name="targetLang"
+          id="targetLang"
+          onChange={(e) => setTargetLang(e.target.value)}
+        />
+        <textarea
+          id="sl"
+          rows="4"
+          cols="50"
+          placeholder="Please enter words to be translated..."
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        ></textarea>
+        <textarea
+          id="tl"
+          rows="4"
+          cols="50"
+          disabled
+          className={styles.translated}
+          value={translatedQuery}
+        ></textarea>
+        <button onClick={translate}>Translate</button>
+      </main>
+      {createDataList()}
     </div>
   );
 }
